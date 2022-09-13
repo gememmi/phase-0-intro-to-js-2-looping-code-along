c715e1cffd31abd62df4eadeffab8418175ddb72
Emily <emelizadaniels@gmail.com>
2022-09-13 15:17:56 -0400

    commit tree 2721017ccfe5f7e2e1a4eadf89272376009e8258

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Emily", "Connor", "Jamie"];
+const event = "surprise";
+
+function writeCards(names, event){
+    let messages = [];
+    for(let i = 0; i < names.length; i++){
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+     messages.push(message); 
+   
+    }
+    return messages;
+}
+writeCards(names,event)
+
+
+
+function countDown() {
+    let x = 10;
+    while (x >= 0) {
+      console.log(x--);
+    }
+   }
+   countDown(x);
